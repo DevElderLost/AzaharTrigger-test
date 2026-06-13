df88ed735883896ef49d25e5cf9d28848a98f747
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-06-13 19:55:21 +0000

    applets: add NetworkApplet HLE stub for C502 Pretendo bypass

--- src/core/hle/service/apt/applet_manager.cpp
+++ src/core/hle/service/apt/applet_manager.cpp
@@ -9,9 +9,11 @@
 #include "core/frontend/input.h"
 #include "core/hle/applets/applet.h"
 #include "core/hle/applets/erreula.h"
+#include "core/hle/applets/network_applet.h"
 #include "core/hle/applets/mii_selector.h"
 #include "core/hle/applets/mint.h"
 #include "core/hle/applets/swkbd.h"
+#include "core/hle/applets/network_applet.h"
 #include "core/hle/service/am/am.h"
 #include "core/hle/service/apt/applet_manager.h"
 #include "core/hle/service/apt/errors.h"
@@ -90,7 +92,8 @@ static constexpr std::array<AppletTitleData, NumApplets> applet_titleids = {{
     {{AppletId::SnoteApp, AppletId::SnoteApp2},
      {0x400300000C402, 0x400300000CC02, 0x400300000D402, 0x400300000C402, 0x400300000DC02,
       0x400300000E202, 0x400300000E802}},
-    {{AppletId::Error, AppletId::Error2},
+    {{AppletId::NetworkApplet, [](...) { return std::make_shared<HLE::Applets::NetworkApplet>(...); }},
+        {AppletId::Error, AppletId::Error2},
      {0x400300000C502, 0x400300000C502, 0x400300000C502, 0x400300000C502, 0x400300000CF02,
       0x400300000CF02, 0x400300000CF02}},
     {{AppletId::Mint, AppletId::Mint2},
